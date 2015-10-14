99d2dbd4f6a132a2f2d4792d68374bf48de89f6e
madhawa-gunasekara <madhawag@wso2.com>
2015-10-14 16:03:40 +0530

    Adding Odata Query test cases

--- modules/integration/tests-integration/tests/src/test/java/org/wso2/dss/integration/test/odata/ODataQueryTestCase.java
+++ modules/integration/tests-integration/tests/src/test/java/org/wso2/dss/integration/test/odata/ODataQueryTestCase.java
@@ -132,4 +132,141 @@ public class ODataQueryTestCase extends DSSIntegrationTest {
 		Assert.assertEquals(response[0], 200);
 		Assert.assertFalse(response[1].toString().contains("Singapore"));
 	}
+
+	@Test(groups = "wso2.dss", description = "filter query test with not equal operator")
+	public void validateFilterWithNotEqualQueryTestCase() throws Exception {
+		String endpoint = webAppUrl + "/odata/" + serviceName + "/" + configId + "/CUSTOMERS?$filter=(COUNTRY%20ne%20%27Singapore%27)";
+		Map<String, String> headers = new HashMap<>();
+		headers.put("Accept", "application/json");
+		Object[] response = sendGET(endpoint, headers);
+		Assert.assertEquals(response[0], 200);
+		Assert.assertFalse(response[1].toString().contains("Singapore"));
+	}
+
+	@Test(groups = "wso2.dss", description = "filter query test with endswith operator")
+	public void validateFilterWith_EndsWithQueryTestCase() throws Exception {
+		String endpoint = webAppUrl + "/odata/" + serviceName + "/" + configId + "/CUSTOMERS?$filter=endswith(CUSTOMERNAME,'graphique')";
+		Map<String, String> headers = new HashMap<>();
+		headers.put("Accept", "application/json");
+		Object[] response = sendGET(endpoint, headers);
+		Assert.assertEquals(response[0], 200);
+		Assert.assertTrue(response[1].toString().contains("graphique"));
+	}
+
+	@Test(groups = "wso2.dss", description = "filter query test with greater than operator")
+	public void validateFilterWithGreaterThanQueryTestCase() throws Exception {
+		String endpoint = webAppUrl + "/odata/" + serviceName + "/" + configId + "/CUSTOMERS?$filter=CREDITLIMIT%20gt%2021000";
+		Map<String, String> headers = new HashMap<>();
+		headers.put("Accept", "application/json");
+		Object[] response = sendGET(endpoint, headers);
+		Assert.assertEquals(response[0], 200);
+		Assert.assertFalse(response[1].toString().contains("21000"));
+		Assert.assertFalse(response[1].toString().contains("\"CREDITLIMIT\":0.0"));
+	}
+
+	@Test(groups = "wso2.dss", description = "filter query test with greater than or equal operator")
+	public void validateFilterWithGreaterThanOrEqualQueryTestCase() throws Exception {
+		String endpoint = webAppUrl + "/odata/" + serviceName + "/" + configId + "/CUSTOMERS?$filter=CREDITLIMIT%20ge%2021000";
+		Map<String, String> headers = new HashMap<>();
+		headers.put("Accept", "application/json");
+		Object[] response = sendGET(endpoint, headers);
+		Assert.assertEquals(response[0], 200);
+		Assert.assertTrue(response[1].toString().contains("21000"));
+		Assert.assertFalse(response[1].toString().contains("\"CREDITLIMIT\":0.0"));
+	}
+
+	@Test(groups = "wso2.dss", description = "filter query test with less than operator")
+	public void validateFilterWithLessThanQueryTestCase() throws Exception {
+		String endpoint = webAppUrl + "/odata/" + serviceName + "/" + configId + "/CUSTOMERS?$filter=CREDITLIMIT%20lt%2021000";
+		Map<String, String> headers = new HashMap<>();
+		headers.put("Accept", "application/json");
+		Object[] response = sendGET(endpoint, headers);
+		Assert.assertEquals(response[0], 200);
+		Assert.assertFalse(response[1].toString().contains("21000"));
+		Assert.assertTrue(response[1].toString().contains("\"CREDITLIMIT\":0.0"));
+	}
+
+	@Test(groups = "wso2.dss", description = "filter query test with less than or equal operator")
+	public void validateFilterWithLessThanOrEqualQueryTestCase() throws Exception {
+		String endpoint = webAppUrl + "/odata/" + serviceName + "/" + configId + "/CUSTOMERS?$filter=CREDITLIMIT%20le%2021000";
+		Map<String, String> headers = new HashMap<>();
+		headers.put("Accept", "application/json");
+		Object[] response = sendGET(endpoint, headers);
+		Assert.assertEquals(response[0], 200);
+		Assert.assertTrue(response[1].toString().contains("21000"));
+		Assert.assertTrue(response[1].toString().contains("\"CREDITLIMIT\":0.0"));
+	}
+
+	@Test(groups = "wso2.dss", description = "filter query test with and operator")
+	public void validateFilterWithAndQueryTestCase() throws Exception {
+		String endpoint = webAppUrl + "/odata/" + serviceName + "/" + configId + "/CUSTOMERS?$filter=CREDITLIMIT%20lt%2021000%20and%20CREDITLIMIT%20gt%20500";
+		Map<String, String> headers = new HashMap<>();
+		headers.put("Accept", "application/json");
+		Object[] response = sendGET(endpoint, headers);
+		Assert.assertEquals(response[0], 200);
+		Assert.assertTrue(response[1].toString().contains("11000"));
+		Assert.assertFalse(response[1].toString().contains("\"CREDITLIMIT\":0.0"));
+	}
+
+	@Test(groups = "wso2.dss", description = "filter query test with or operator")
+	public void validateFilterWithOrQueryTestCase() throws Exception {
+		String endpoint = webAppUrl + "/odata/" + serviceName + "/" + configId + "/CUSTOMERS?$filter=CREDITLIMIT%20lt%2021000%20or%20CREDITLIMIT%20gt%20500";
+		Map<String, String> headers = new HashMap<>();
+		headers.put("Accept", "application/json");
+		Object[] response = sendGET(endpoint, headers);
+		Assert.assertEquals(response[0], 200);
+		Assert.assertTrue(response[1].toString().contains("71800"));
+		Assert.assertTrue(response[1].toString().contains("\"CREDITLIMIT\":0.0"));
+	}
+
+
+	@Test(groups = "wso2.dss", description = "filter query test with addition operator")
+	public void validateFilterWithAdditionQueryTestCase() throws Exception {
+		String endpoint = webAppUrl + "/odata/" + serviceName + "/" + configId + "/CUSTOMERS?$filter=(CREDITLIMIT%20add%205)%20lt%206%20and%20(CREDITLIMIT%20add%205)%20gt%202";
+		Map<String, String> headers = new HashMap<>();
+		headers.put("Accept", "application/json");
+		Object[] response = sendGET(endpoint, headers);
+		Assert.assertEquals(response[0], 200);
+		Assert.assertTrue(response[1].toString().contains("\"CREDITLIMIT\":0.0"));
+	}
+
+	@Test(groups = "wso2.dss", description = "filter query test with subtraction operator")
+	public void validateFilterWithSubtractionQueryTestCase() throws Exception {
+		String endpoint = webAppUrl + "/odata/" + serviceName + "/" + configId + "/CUSTOMERS?$filter=(CREDITLIMIT%20sub%205)%20lt%20-1";
+		Map<String, String> headers = new HashMap<>();
+		headers.put("Accept", "application/json");
+		Object[] response = sendGET(endpoint, headers);
+		Assert.assertEquals(response[0], 200);
+		Assert.assertTrue(response[1].toString().contains("\"CREDITLIMIT\":0.0"));
+	}
+
+	@Test(groups = "wso2.dss", description = "filter query test with multiplication operator")
+	public void validateFilterWithMultiplicationQueryTestCase() throws Exception {
+		String endpoint = webAppUrl + "/odata/" + serviceName + "/" + configId + "/CUSTOMERS?$filter=(CREDITLIMIT%20add%201)%20mul%205%20lt%206%20and%20(CREDITLIMIT%20add%201)%20mul%205%20gt%204";
+		Map<String, String> headers = new HashMap<>();
+		headers.put("Accept", "application/json");
+		Object[] response = sendGET(endpoint, headers);
+		Assert.assertEquals(response[0], 200);
+		Assert.assertTrue(response[1].toString().contains("\"CREDITLIMIT\":0.0"));
+	}
+
+	@Test(groups = "wso2.dss", description = "filter query test with division operator")
+	public void validateFilterWithDivisionQueryTestCase() throws Exception {
+		String endpoint = webAppUrl + "/odata/" + serviceName + "/" + configId + "/CUSTOMERS?$filter=(CREDITLIMIT%20add%2010)%20div%205%20gt%201%20and%20(CREDITLIMIT%20add%2010)%20div%205%20lt%203";
+		Map<String, String> headers = new HashMap<>();
+		headers.put("Accept", "application/json");
+		Object[] response = sendGET(endpoint, headers);
+		Assert.assertEquals(response[0], 200);
+		Assert.assertTrue(response[1].toString().contains("\"CREDITLIMIT\":0.0"));
+	}
+
+	@Test(groups = "wso2.dss", description = "filter query test with modulus operator")
+	public void validateFilterWithModulusQueryTestCase() throws Exception {
+		String endpoint = webAppUrl + "/odata/" + serviceName + "/" + configId + "/CUSTOMERS?$filter=(CREDITLIMIT%20add%2010)%20div%205%20gt%201%20and%20(CREDITLIMIT%20add%2010)%20div%205%20lt%203";
+		Map<String, String> headers = new HashMap<>();
+		headers.put("Accept", "application/json");
+		Object[] response = sendGET(endpoint, headers);
+		Assert.assertEquals(response[0], 200);
+		Assert.assertTrue(response[1].toString().contains("\"CREDITLIMIT\":0.0"));
+	}
 }

c11d44974d64572089da7c8bab7050fa44917cdf
madhawa-gunasekara <madhawag@wso2.com>
2015-11-23 18:25:14 +0530

    Fixing test failure and adding DS937 test case

--- modules/integration/tests-integration/tests/src/test/java/org/wso2/dss/integration/test/sparql/SPARQLServiceTestCase.java
+++ modules/integration/tests-integration/tests/src/test/java/org/wso2/dss/integration/test/sparql/SPARQLServiceTestCase.java
@@ -18,24 +18,29 @@
 
 package org.wso2.dss.integration.test.sparql;
 
-import org.apache.axiom.om.OMAbstractFactory;
-import org.apache.axiom.om.OMElement;
-import org.apache.axiom.om.OMFactory;
-import org.apache.axiom.om.OMNamespace;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import org.wso2.carbon.automation.test.utils.axis2client.AxisServiceClient;
 import org.wso2.dss.integration.test.DSSIntegrationTest;
 
 import javax.activation.DataHandler;
 import javax.xml.xpath.XPathExpressionException;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
-import java.rmi.RemoteException;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.testng.Assert.assertTrue;
 
@@ -44,11 +49,12 @@ public class SPARQLServiceTestCase extends DSSIntegrationTest {
     private static final Log log = LogFactory.getLog(SPARQLServiceTestCase.class);
 
     private final String serviceName = "SPARQLDataService";
+    private  String serviceEndPoint;
 
     @BeforeClass(alwaysRun = true)
     public void serviceDeployment() throws Exception {
         super.init();
-        String serviceEndPoint = getServiceUrlHttp(serviceName);
+        serviceEndPoint = getServiceUrlHttp(serviceName);
         String resourceFileLocation = getResourceLocation();
         deployService(serviceName,
                 new DataHandler(new URL("file:///" + resourceFileLocation +File.separator + "dbs" + File.separator +
@@ -63,18 +69,53 @@ public class SPARQLServiceTestCase extends DSSIntegrationTest {
     }
 
     @Test(groups = {"wso2.dss"})
-    public void getAllBookmarkData() throws RemoteException, XPathExpressionException {
+    public void getAllBookmarkData() throws IOException, XPathExpressionException {
+        String endpoint = serviceEndPoint + ".SOAP11Endpoint/";
+        String content ="<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:dat=\"http://ws.wso2.org/dataservice\">\n" +
+                        "   <soapenv:Header/>\n" +
+                        "   <soapenv:Body>\n" +
+                        "      <dat:getBookmarks/>\n" +
+                        "   </soapenv:Body>\n" +
+                        "</soapenv:Envelope>";
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Accept", "application/xml");
+        headers.put("Content-Type", "text/plain");
+        headers.put("SOAPAction", "\"urn:getBookmarks\"");
+        Object[] response = sendPOST(endpoint, content, headers);
+        Assert.assertEquals(Integer.parseInt(response[0].toString()), 200);
+        log.info("Response : " + response[1].toString());
+        Assert.assertTrue(response[1].toString().contains("<bookmark>"), "Expected Result not found on response message");
+        Assert.assertTrue(response[1].toString().contains("http://semantic.eea.europa.eu/home/roug/bookmarks"), "Expected Result not found on response message");
 
-        OMFactory fac = OMAbstractFactory.getOMFactory();
-        OMNamespace omNs = fac.createOMNamespace("http://ws.wso2.org/dataservice", "ns1");
-        OMElement payload = fac.createOMElement("getBookmarks", omNs);
+    }
 
-        OMElement result = new AxisServiceClient().sendReceive(payload, getServiceUrlHttp(serviceName), "getBookmarks");
-        log.info("Response : " + result);
-        Assert.assertTrue(result.toString().contains("<bookmark>"), "Expected Result not found on response message");
-        Assert.assertTrue(result.toString().contains("http://semantic.eea.europa.eu/home/roug/bookmarks"),
-                "Expected Result not found on response message");
-
+    public Object[] sendPOST(String endpoint, String content, Map<String, String> headers) throws IOException {
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpPost httpPost = new HttpPost(endpoint);
+        httpClient.getParams().setParameter("http.socket.timeout", 300000);
+        for (String headerType : headers.keySet()) {
+            httpPost.setHeader(headerType, headers.get(headerType));
+        }
+        if (content != null) {
+            HttpEntity httpEntity = new ByteArrayEntity(content.getBytes("UTF-8"));
+            if (headers.get("Content-Type") == null) {
+                httpPost.setHeader("Content-Type", "application/json");
+            }
+            httpPost.setEntity(httpEntity);
+        }
+        HttpResponse httpResponse = httpClient.execute(httpPost);
+        if (httpResponse.getEntity() != null) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(httpResponse.getEntity().getContent()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = reader.readLine()) != null) {
+                response.append(inputLine);
+            }
+            reader.close();
+            return new Object[] { httpResponse.getStatusLine().getStatusCode(), response.toString() };
+        } else {
+            return new Object[] { httpResponse.getStatusLine().getStatusCode() };
+        }
     }
 
     @AfterClass(alwaysRun = true, groups = "wso2.dss", description = "delete service")

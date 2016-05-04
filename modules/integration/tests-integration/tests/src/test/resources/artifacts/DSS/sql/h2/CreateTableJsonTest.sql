509022b3f8159f97403d43642a1f1f4586d75609
anupama-pathirage <anupama@wso2.com>
2016-05-04 14:39:00 +0530

    DS-1190,DS-1192-Adding Integration Tests-Reformatting SQL File

--- modules/integration/tests-integration/tests/src/test/resources/artifacts/DSS/sql/h2/CreateTableJsonTest.sql
+++ modules/integration/tests-integration/tests/src/test/resources/artifacts/DSS/sql/h2/CreateTableJsonTest.sql
@@ -1,1 +1,7 @@
-CREATE TABLE Persons(PersonID int,LastName varchar(255),FirstName varchar(255),Address varchar(255),Image blob);
+CREATE TABLE Persons(
+  PersonID int,
+  LastName varchar(255),
+  FirstName varchar(255),
+  Address varchar(255),
+  Image blob
+  );
